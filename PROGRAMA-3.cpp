b7d5d1ee52e4e7dfc639cf342dbafe00e8f74ba7
Erika-cmd <zenaidavalenciacondori@gmail.com>
2025-04-17 23:19:54 -0500

    Add files via upload

---
+++ PROGRAMA-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int area (int radio) {
+	int pi = 3.14;
+	int res = pi * (radio*radio);
+	return res;
+}
+int main() {
+	int p;
+	cout <<"Ingrese el radio" << endl;
+	cin >> p;
+	int respuesta = area (p);
+	cout << respuesta;
+	return 0;
+}
