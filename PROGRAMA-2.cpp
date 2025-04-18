e2e8a841f195bb52d41d8ae48a6412c1ba167712
Erika-cmd <zenaidavalenciacondori@gmail.com>
2025-04-17 23:19:34 -0500

    Add files via upload

---
+++ PROGRAMA-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int nota;
+	cout << "Ingrese su Nota" << endl;
+	cin >> nota;
+	
+	if (nota > 10) {
+		cout <<"Aprobado" << endl;
+	} else {
+		cout << "Desaprobado" << endl;
+	}
+	
+	return 0;
+}
